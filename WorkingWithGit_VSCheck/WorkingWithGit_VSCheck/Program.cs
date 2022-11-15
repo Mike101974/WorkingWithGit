5d16be0cf7a6fa3c89456e4cca6dcd72d0367b60
PETA Training in GIT <petatrain.git@gmail.com>
2022-11-15 21:29:45 +0000

    First commit - from command line

---
+++ WorkingWithGit_VSCheck/WorkingWithGit_VSCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithGit_VSCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
